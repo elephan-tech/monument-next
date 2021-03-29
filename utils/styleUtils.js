18d4a2caa45dadad1a2ad7272f47b29a005e14a9
pablo <pa.romerorgz@gmail.com>
2021-03-29 15:09:29 -0500

    update styles

--- utils/styleUtils.js
+++ utils/styleUtils.js
@@ -1,10 +1,10 @@
 import isObject from "lodash/isObject";
 import theme from "../styles/theme";
 
-export const getColor = (color) => {
+export const getColor = (color, shade = "main") => {
   const isHex = color && color.includes("#");
   const currentColor = isHex ? color : theme.colors[color];
   return isObject(currentColor)
-    ? currentColor.main
+    ? currentColor[shade]
     : currentColor || theme.colors.medium;
 };
