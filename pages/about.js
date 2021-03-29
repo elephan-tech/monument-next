3ceb361c8a1319689d73f53381fe06bec639c591
Ivan Romero <ivanrr308@gmail.com>
2021-03-29 12:29:48 -0500

    Brush Stroke & Link Componentes

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Page from "../components/Page";
+import BrushStroke from '../components/BrushStroke.jsx';
+import HoverLink from '../components/HoverLink.jsx';
+
+const brushStrokeProps  = { 
+  word: 'A Home',
+  fill: '#fed02f'
+}
 
 const about = () => {
   return (
     <Page title="About" description="">
-      About content
+      <div> <div style={{color: "#042f54", fontWeight:"bold", fontSize:"4em"}}> More Than Just A School..</div> < BrushStroke word={brushStrokeProps.word} fill={brushStrokeProps.fill}/></div>
+      <br/>
+      < HoverLink word='This is a clickable link' link='/'/>
     </Page>
   );
 };
