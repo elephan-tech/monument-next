9dafe071b0c1b32efdff25690de09d14269a550a
pablo <pa.romerorgz@gmail.com>
2021-04-01 18:00:19 -0500

    remove cont

--- components/Page.jsx
+++ components/Page.jsx
@@ -1,6 +1,3 @@
-import Container from "@material-ui/core/Container";
-import Fab from "@material-ui/core/Fab";
-import EditIcon from "@material-ui/icons/CalendarTodayOutlined";
 import React from "react";
 import pages from "../config/pages";
 import Meta from "./Meta";
@@ -12,12 +9,7 @@ const Page = ({ children, title, description = "" }) => {
       <Meta title={title} description={description} />
       <Navbar pages={pages} title={title}></Navbar>
 
-      <Container maxWidth="xl" disableGutters>
-        <Fab color="secondary" aria-label="calendar" size="large">
-          <EditIcon />
-        </Fab>
-        {children}
-      </Container>
+      {children}
     </>
   );
 };
