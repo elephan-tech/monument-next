a6078ee4ee9f2ff9ccfbab713d878ffae05fab93
elbgi <elbgi@monsanto.com>
2021-04-05 01:34:50 -0500

    update about section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import  Grid  from "@material-ui/core/Grid";
 import BrushStroke from "../components/BrushStroke";
 import Page from "../components/Page";
 import Section from "../components/section";
+import Image from 'next/image'
+import { Button, Typography } from "@material-ui/core";
+import Heading from "../components/heading";
 
 export default function Home() {
   return (
@@ -9,50 +13,66 @@ export default function Home() {
         backgroundImage="/bg_columns.png"
         backgroundPosition="center"
         backgroundSize="cover"
+        topMargin
       >
-        <div width={1 / 2}>
-          More than just a school
-          <BrushStroke BrushStroke fill={"#310494"}>
-            A Home
-          </BrushStroke>
-        </div>
+      <Grid container alignItems="center" justify="center">
+        <Grid item xs={12} sm={12} md={6} lg={8} xl={8}>
+         {/* <Image src="/IMG_1905.png" width="auto" height="auto" layout="responsive"></Image> */}
+        </Grid>
+        <Grid item container xs={12} sm={12} md={6} lg={4} xl={4} alignItems="center" justify="center" direction="column">
+          <Typography variant="h3">More than a just a school,  <span style={{
+           
+              background: "url('/brush1.png')",
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: '50% 110%',
+              backgroundPosition:'center',
+              padding: '8px 0'
+          }}> a home</span></Typography>
+        </Grid>
+      </Grid>
       </Section>
-      <Section
+      {/* {/* <Section
         backgroundImage="/bg_home_section2.jpeg"
         backgroundPosition="center"
         backgroundSize="cover"
       >
-        <div>
-          <div>
-            Monument Academy Public Charter School is one-of-a-kind Boarding
-            School located in the nation's Capital.
-          </div>
-          <div>
-            <button>Learn More</button>
-          </div>
-        </div>
-      </Section>
+        <Grid container alignItems="flex-end" justify='space-between'>
+         <Grid item   xs={12} sm={12} md={6} lg={6} xl={6}>
+        <Typography color="light" variant='h5'> Monument Academy Public Charter School is one-of-a-kind Boarding
+            School located in the nation's Capital.</Typography>
+         </Grid>
+         <Grid item xs={12} sm={12} md={6} lg={6} xl={6} alignItems="flex-end">
+        <Button variant="contained" color="primary">Learn More</Button>
+         </Grid>
+        </Grid>
+      </Section> */}
       <Section background="secondary">
-        <div>
-          <div>
-            Monument Academy Public Charter School is one-of-a-kind Boarding
-            School located in the nation's Capital.
-          </div>
-          <div>
-            <button>Learn More</button>
-          </div>
-        </div>
+        <Grid container direction="row">
+          <Grid item container direction="column" justify="space-evenly" alignItems="flex-start">
+            <Grid item>
+            <Heading color="primary" decoration="accent"> Our Mission </Heading>
+              <Typography variant="body1">Lorem Ipsum</Typography>
+            </Grid>
+            <Grid item>
+            <Heading color="primary" decoration="accent">We change lives</Heading>
+              <Typography variant="body1">Lorem Ipsum</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
       </Section>
-      <Section bg="white" title="landing" position="left center" bgSize="cover">
-        <div>
-          <div>
-            Monument Academy Public Charter School is one-of-a-kind Boarding
-            School located in the nation's Capital.
-          </div>
-          <div>
-            <button>Learn More</button>
-          </div>
-        </div>
+      <Section>
+        <Grid container direction="row">
+          <Grid item container direction="column" justify="space-evenly" alignItems="flex-start">
+            <Grid item>
+            <Heading color="primary" decoration="secondary"> Our Mission </Heading>
+              <Typography variant="body1">Lorem Ipsum</Typography>
+            </Grid>
+            <Grid item>
+            <Heading color="primary" decoration="secondary">We change lives</Heading>
+              <Typography variant="body1">Lorem Ipsum</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
       </Section>
     </Page>
   );
