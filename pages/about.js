20dba551185bee558b5ea01c8a30fcdd15425d7e
pablo <pa.romerorgz@gmail.com>
2021-04-06 00:48:46 -0500

    fix imports

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,15 @@
 import React from "react";
 import Page from "../components/Page";
-import Section from '../components/Section';
 
-const brushStrokeProps  = { 
-  word: 'A Home',
-  fill: '#fed02f'
-}
+const brushStrokeProps = {
+  word: "A Home",
+  fill: "#fed02f",
+};
 
 const about = () => {
   return (
     <Page title="About" description="">
-      <Section topMargin> hello </Section>
+      about
     </Page>
   );
 };
